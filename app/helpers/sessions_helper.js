19f39fec8427544bf166b3b8fcba6f514e538736
KINOMOTO, Masakazu <kino2718@gmail.com>
2025-09-30 16:32:23 +0900

    8.2.2 現在のユーザー

--- app/helpers/sessions_helper.js
+++ app/helpers/sessions_helper.js
@@ -1,7 +1,19 @@
+const User = require('../models/user')
+
 function logIn(session, user) {
     session.userId = user.id
 }
 
+let _currentUser
+function currentUser(session) {
+    if (session.userId) {
+        _currentUser = _currentUser || User.find(session.userId)
+        return _currentUser
+    }
+    return null
+}
+
 module.exports = {
     logIn,
+    currentUser,
 }
