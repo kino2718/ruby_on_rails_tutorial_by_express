45aa1c422be212d4a4bea0e5af331d5c7019d4bc
KINOMOTO, Masakazu <kino2718@gmail.com>
2025-10-07 08:22:33 +0900

    csrf secretの保存場所をsessionからcookieに変更。

--- app/helpers/csrf_helper.js
+++ app/helpers/csrf_helper.js
@@ -1,23 +1,33 @@
 const Tokens = require('csrf')
 const tokens = new Tokens()
 
+const CSRF_SECRET = 'csrf_secret'
+
 // csrf token発行
 function makeCsrfToken(req, res, next) {
-    if (!req.session.csrfSecret) {
-        // sessionにcsrfSecretを登録。これによりcsrf secretがsessionに紐付けされる
-        // つまりsession idが漏れなければcsrf token自体が漏れても問題無い
-        req.session.csrfSecret = tokens.secretSync()
+    // cookieからcsrfSecretを取得
+    let csrfSecret = req.cookies[CSRF_SECRET]
+    if (!csrfSecret) {
+        // 無ければcookieにcsrfSecretを登録
+        csrfSecret = tokens.secretSync()
+        res.cookie(CSRF_SECRET, csrfSecret, {
+            httpOnly: true,                                 // JSからアクセス不可
+            secure: process.env.NODE_ENV === 'production',  // 製品版ではHTTPSのみ送信
+            sameSite: 'lax',                                // CSRF対策
+        }
+        )
     }
-    res.locals.csrfToken = tokens.create(req.session.csrfSecret)
+    res.locals.csrfToken = tokens.create(csrfSecret)
     next()
 }
 
 // csrf token検証
 function verifyCsrfToken(req, res, next) {
+    // csrf tokenを取得
     const token = req.body._csrf || req.query._csrf
-
-    // sessionに登録したcsrf secretでtokenを検証
-    if (!tokens.verify(req.session.csrfSecret, token)) {
+    // cookieに登録したcsrf secretでtokenを検証
+    const secret = req.cookies[CSRF_SECRET]
+    if (!secret || !tokens.verify(secret, token)) {
         return res.status(403).send('invalid request')
     }
     next()
