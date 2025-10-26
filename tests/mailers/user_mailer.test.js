99f6d72620add5567bbad4f074546637de740d06
KINOMOTO, Masakazu <kino2718@gmail.com>
2025-10-26 12:15:38 +0900

    12.2.2 送信メールのテスト

--- tests/mailers/user_mailer.test.js
+++ tests/mailers/user_mailer.test.js
@@ -1,5 +1,7 @@
 const User = require('../../app/models/user')
 const userMailer = require('../../app/mailers/user_mailer')
+const knexUtils = require('../../app/db/knex_utils')
+const knex = knexUtils.knex
 
 describe('user mailer test', () => {
     let user
@@ -10,6 +12,7 @@ describe('user mailer test', () => {
             email: 'michael@example.com',
         })
         user.activationToken = await User.newToken()
+        user.resetToken = await User.newToken()
     })
 
     test('account activation', async () => {
@@ -23,5 +26,20 @@ describe('user mailer test', () => {
         expect(mail.html).toContain(user.activationToken)
         expect(mail.text).toContain(encodeURIComponent(user.email))
         expect(mail.html).toContain(encodeURIComponent(user.email))
+    })
+
+    test('password reset', async () => {
+        const mail = await userMailer.passwordReset(user, 'localhost:3000')
+        expect(mail.subject).toBe('Password reset')
+        expect(mail.to).toBe(user.email)
+        expect(mail.from).toBe('user@realdomain.com')
+        expect(mail.text).toContain(user.resetToken)
+        expect(mail.html).toContain(user.resetToken)
+        expect(mail.text).toContain(encodeURIComponent(user.email))
+        expect(mail.html).toContain(encodeURIComponent(user.email))
+    })
+
+    afterAll(async () => {
+        await knex.destroy() // コネクションを閉じる
     })
 })
