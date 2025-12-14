4eefaeb701e517c84f76041058aa7b61ac0769c6
KINOMOTO, Masakazu <kino2718@gmail.com>
2025-12-14 11:53:42 +0900

    14.2.1 フォローのサンプルデータ

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -6,41 +6,50 @@ const User = require('../app/models/user')
  * @returns { Promise<void> }
  */
 exports.seed = async function (knex) {
-  // Deletes ALL existing entries
-  await knex('users').del()
-
-  // メインのサンプルユーザーを1人作成する
-  await User.create({
-    name: 'Example User',
-    email: 'example@railstutorial.org',
-    password: 'foobar',
-    passwordConfirmation: 'foobar',
-    admin: true,
-    activated: true,
-    activatedAt: knex.fn.now()
-  })
-
-  // 追加のユーザーをまとめて生成する
-  for (let i = 1; i < 100; ++i) {
-    const name = faker.person.fullName()
-    const email = `example-${i}@railstutorial.org`
-    const password = 'password'
+    // メインのサンプルユーザーを1人作成する
     await User.create({
-      name: name,
-      email: email,
-      password: password,
-      passwordConfirmation: password,
-      activated: true,
-      activatedAt: knex.fn.now()
+        name: 'Example User',
+        email: 'example@railstutorial.org',
+        password: 'foobar',
+        passwordConfirmation: 'foobar',
+        admin: true,
+        activated: true,
+        activatedAt: knex.fn.now()
     })
-  }
 
-  // idの順で6人にmicropostsを生成する
-  const users = await User.take(6)
-  for (let i = 0; i < 50; ++i) {
-    const content = faker.lorem.sentence(5)
-    for (const user of users) {
-      await user.microposts.create({ content: content })
+    // 追加のユーザーをまとめて生成する
+    for (let i = 1; i < 100; ++i) {
+        const name = faker.person.fullName()
+        const email = `example-${i}@railstutorial.org`
+        const password = 'password'
+        await User.create({
+            name: name,
+            email: email,
+            password: password,
+            passwordConfirmation: password,
+            activated: true,
+            activatedAt: knex.fn.now()
+        })
     }
-  }
+
+    // idの順で6人にmicropostsを生成する
+    let users = await User.take(6)
+    for (let i = 0; i < 50; ++i) {
+        const content = faker.lorem.sentence(5)
+        for (const user of users) {
+            await user.microposts.create({ content: content })
+        }
+    }
+
+    // ユーザーフォローのリレーションシップを作成する
+    users = await User.all()
+    const user = users[0]
+    const following = users.slice(2, 51)
+    const followers = users.slice(3, 41)
+    for (const followed of following) {
+        await user.follow(followed)
+    }
+    for (const follower of followers) {
+        await follower.follow(user)
+    }
 }
